1f52713adf17943f5fb5520b17aa898e717a215c
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-11 22:36:00 +0200

    *

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,28 +1,27 @@
-// Gioco dei dadi
+// GIOCO DEI DADI
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-
-//SOLUZIONE
-//ricevo in input il nome dei due utenti e creo per ciascuno una variabile
-const user1_name = prompt("utente 1 inserisci il tuo nome");
-const user2_name = prompt("utente 2 inserisci il tuo nome");
-//utilizzo la funzione math.random ed assegno il valore al primo utente
-const user1_d6 = (Math.floor(Math.random() * 6) + 1);
-console.log(user1_name, " ha lanciato ",user1_d6);
-const user1 = [user1_name, user1_d6];
-//utilizzo la funzione math.random ed assegno il valore al secondo utente
-const user2_d6 = (Math.floor(Math.random() * 6) + 1);
-console.log(user2_name, " ha lanciato ",user2_d6);
-const user2 = [user2_name, user2_d6];
-//confronto tra loro i valori il valore più alto vince, comunico in output il vincitore
-let msg = "";
-if (user1[1] === user2[1]) {
-    msg = "pareggio";
-} else if (user1[1] > user2[1]) {
-    msg = `${"l' utente "}${user1[0]}${" ha vinto, con un lancio pari a "}${user1[1]}${", contro un lancio avversario pari "}${user2[1]} `
-} else {
-    msg = `${"l' utente "}${user2[0]}${" ha vinto, con un lancio pari a "}${user2[1]}${", contro un lancio avversario pari "}${user1[1]} `
-}
-//stampo il msg
-alert(msg);
+        // Math.floor(Math.random() * 6) + 1
+        let msg
+        const pc = Math.floor(Math.random() * 6) + 1;
+        const user = [];
+        const btn = document.getElementById("submit");
+        
+        btn.addEventListener ("click", function() {
+            let username = document.getElementById("username");
+            user.push(username.value);
+            const userdice = Math.floor(Math.random() * 6) + 1;
+            user.push(userdice);
+        // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+            if (pc === user[1]) {
+                msg = "pareggio"
+            } else if (pc > user[1]) {
+                msg = ` ha vinto il pc con un lancio pari a ${pc} contro ${user[1]} `
+            } else {
+                msg = ` ha vinto ${user[0]}, con un lancio pari a ${user[1]} contro ${pc} `
+            }
+        
+            console.log("lancio del pc ", pc);
+            console.log("lancio di ", user[0], " pari a ", user[1]);
+            alert(msg);
+        })
+        
