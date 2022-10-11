ad72dfd5644b359381e00ac98bf38ed43f3e6861
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-11 14:27:39 +0200

    scaffolding

---
+++ mail/js/script.js
@@ -0,0 +1,10 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+//utente inserisci mail
+//creo array mails utenti con permesso di accesso
+//ciclo for scorro il valore inserito dall' utente nella lista mails
+//se la mail è giusta garantisco l' accesso all' utente
