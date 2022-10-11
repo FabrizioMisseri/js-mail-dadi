f5054c76c78203b121f35bb253eb49eeb97645c4
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-11 15:20:15 +0200

    take inputs, roll dices

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,3 +1,17 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+//SOLUZIONE
+//ricevo in input il nome dei due utenti e creo per ciascuno una variabile
+const user1 = prompt("utente 1 inserisci il tuo nome");
+const user2 = prompt("utente 2 inserisci il tuo nome");
+//utilizzo la funzione math.random ed assegno il valore al primo utente
+const user1_d6 = (Math.floor(Math.random() * 6) + 1);
+console.log("risultato 1 ",user1_d6);
+//utilizzo la funzione math.random ed assegno il valore al secondo utente
+const user2_d6 = (Math.floor(Math.random() * 6) + 1);
+console.log("risultato 2 ",user2_d6);
+//confronto tra loro i valori
+//il valore più alto vince, comunico in output il vincitore
