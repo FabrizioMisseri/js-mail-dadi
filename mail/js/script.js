8fd1fcf6454ce328d41f3d6c2a567d8c86956f31
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-11 14:51:15 +0200

    mail JS code

--- mail/js/script.js
+++ mail/js/script.js
@@ -5,6 +5,22 @@
 
 
 //utente inserisci mail
+let msg = false;
+const usermail = prompt("inserisci la tua mail");
 //creo array mails utenti con permesso di accesso
+const listmail = ["bat_man@gmail.com", "bat-man@gmail.com", "batman@gmail.com", "BATMAN@gmail.com"];
 //ciclo for scorro il valore inserito dall' utente nella lista mails
+for (let i = 0; i < listmail.length; i++) {
+    if (usermail === listmail[i]) {
+        msg = true
+    }
+}
+
 //se la mail è giusta garantisco l' accesso all' utente
+if (msg === true) {
+    msg = "Bentornato BATMAN!";
+} else {
+    msg = "vattene a fanculo... tu non sei BATMAN..."
+}
+
+alert(msg);
