ca10334f3c54b3fd8d0202744025661283fd6d90
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-12 09:40:23 +0200

    change 'const' to 'let'

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,15 +1,16 @@
 // GIOCO DEI DADI
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
         // Math.floor(Math.random() * 6) + 1
-        let msg
-        const pc = Math.floor(Math.random() * 6) + 1;
-        const user = [];
+        let msg;
+        let pc;
+        let user = [];
         const btn = document.getElementById("submit");
         
         btn.addEventListener ("click", function() {
+            pc = Math.floor(Math.random() * 6) + 1;
             let username = document.getElementById("username");
             user.push(username.value);
-            const userdice = Math.floor(Math.random() * 6) + 1;
+            let userdice = Math.floor(Math.random() * 6) + 1;
             user.push(userdice);
         // Stabilire il vincitore, in base a chi fa il punteggio più alto.
             if (pc === user[1]) {
